8733f8df722827fd2cc9b53b3b97afb1961b47ef
LTC-Audiovisuales <ltambella@gmail.com>
2026-08-27 20:40:37 -0300

    Carga inicial de la plataforma
    Primera versión de la plataforma con Mulun Hunter y la estructura para incorporar los trabajos del estudiantado.

---
+++ juegos.js
@@ -0,0 +1,8 @@
+window.JUEGOS = [
+  {
+    titulo: "Mulun Hunter",
+    autor: "Luciano Tambella",
+    portada: "juegos/mulun-hunter/assets/portada_mulun_hunter.png",
+    juego: "juegos/mulun-hunter/Juego.html"
+  }
+];
