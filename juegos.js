8779ebb2245b49cd771186ad0f9679c23db579e8
LTC-Audiovisuales <ltambella@gmail.com>
2026-08-27 22:28:29 -0300

    Juego mío y el base

---
+++ juegos.js
@@ -0,0 +1,14 @@
+window.JUEGOS = [
+  {
+    titulo: "Mulun Hunter",
+    autor: "Luciano Tambella",
+    portada: "juegos/mulun-hunter/assets/portada_mulun_hunter.png",
+    juego: "juegos/mulun-hunter/Juego.html"
+  },
+  {
+    titulo: "Juego Base",
+    autor: "Luciano Tambella · Plantilla de clase",
+    portada: "juegos/juego-base/assets/portada_juego_base.svg",
+    juego: "juegos/juego-base/Juego.html"
+  }
+];
