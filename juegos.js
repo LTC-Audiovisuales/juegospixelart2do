5f7cd8bab923d1ebab59a2041531ebb950f823fd
LTC-Audiovisuales <ltambella@gmail.com>
2026-08-27 22:28:11 -0300

    Delete juegos.js

--- juegos.js
+++
@@ -1,8 +0,0 @@
-window.JUEGOS = [
-  {
-    titulo: "Mulun Hunter",
-    autor: "Luciano Tambella",
-    portada: "juegos/mulun-hunter/assets/portada_mulun_hunter.png",
-    juego: "juegos/mulun-hunter/Juego.html"
-  }
-];
